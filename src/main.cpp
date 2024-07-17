de6c774add8ba93bd86dd6d6de58692d93e198a5
Matias Arocena <m.arocena97@gmail.com>
2024-07-16 22:16:03 -0300

    Compliance with clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 
 #include <array>
 
+const int VERTEX_NUMBER = 9;
+const int WINDOW_WIDTH = 512;
+const int WINDOW_HEIGHT = 512;
+
 GLuint LoadShader(GLenum type, const char *shaderSrc) {
     GLuint shader = glCreateShader(type);
     glShaderSource(shader, 1, &shaderSrc, nullptr);
@@ -18,29 +22,28 @@ GLuint LoadShader(GLenum type, const char *shaderSrc) {
 
 void mainLoop(void* mainLoopArg) 
 {
-    GLfloat vertices[] = {
-        0.0f, 1.0f, 0.0f,  // Top vertex
-        -1.0f,-1.0f, 0.0f,  // Left vertex
-        1.0f,-1.0f, 0.0f   // Right vertex
+    std::array<GLfloat, VERTEX_NUMBER> vertices = {
+        0.0F, 1.0F, 0.0F,  // Top vertex
+        -1.0F,-1.0F, 0.0F,  // Left vertex
+        1.0F,-1.0F, 0.0F   // Right vertex
     };
 
-    SDL_Window* pWindow = (SDL_Window*)mainLoopArg;
+    auto* pWindow = static_cast<SDL_Window*>(mainLoopArg);
     
     GLuint vertexShader = LoadShader(GL_VERTEX_SHADER, "attribute vec4 position; void main() { gl_Position = position; }");
     GLuint fragmentShader = LoadShader(GL_FRAGMENT_SHADER, "precision mediump float; void main() { gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); }");
 
-    // Create and link program
     GLuint program = glCreateProgram();
     glAttachShader(program, vertexShader);
     glAttachShader(program, fragmentShader);
     glLinkProgram(program);
     glUseProgram(program);
 
-    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+    glClearColor(1.0F, 0.0F, 1.0F, 1.0F);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices.data());
 
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glDisableVertexAttribArray(0);
@@ -50,28 +53,18 @@ void mainLoop(void* mainLoopArg)
 
 int main(int argc, char** argv)
 {
-    int winWidth = 512, winHeight = 512;
-
-    // Create SDL window
     SDL_Window *pWindow = 
         SDL_CreateWindow("Hello Triangle Minimal", 
                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                         winWidth, winHeight, 
+                         WINDOW_WIDTH, WINDOW_HEIGHT, 
                          SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetSwapInterval(1);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GLContext glc = SDL_GL_CreateContext(pWindow);
-    printf("INFO: GL version: %s\n", glGetString(GL_VERSION));
+    SDL_GL_CreateContext(pWindow);
 
-    SDL_GL_GetDrawableSize(pWindow, &winWidth, &winHeight);
-    printf("INFO: GL window size = %dx%d\n", winWidth, winHeight);
-    glViewport(0, 0, winWidth, winHeight);   
-
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     void* mainLoopArg = pWindow;
 
@@ -84,7 +77,7 @@ int main(int argc, char** argv)
     while (!quit)
     {
         SDL_Event event;
-        while (SDL_PollEvent(&event))
+        while (SDL_PollEvent(&event) != 0)
         {
             if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
             {
